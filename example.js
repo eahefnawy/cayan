cc65b9a268d67df6723dac2a3da182375ed33a10
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-03-26 22:09:07 +0700

    added support for TRY

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 const { getProjects } = require('./index');
 
 (async () => {
-  const jsonUrl = 'https://gist.githubusercontent.com/eahefnawy/47630049a24f2086a101829263d5384c/raw/cea9846ef2572de155c43f9d8945562a1b8e7312/projects.json'
+  const jsonUrl = 'https://gist.githubusercontent.com/eahefnawy/47630049a24f2086a101829263d5384c/raw/2bf8c9948cc96604f47e4d6fac73fe80de8aee3f/projects.json'
   const projects = await getProjects(jsonUrl)
-  console.log(projects)
+  console.log(projects[0].types['1+1'])
 })()
--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const axios = require('axios')
-const { find, propEq, reduce, keys } = require('ramda')
+const { find, propEq, reduce, keys, map, forEachObjIndexed } = require('ramda')
+const fx = require('money')
 
-const getProjects = (jsonUrl) => axios.get(jsonUrl).then(res => res.data)
+const getProjects = async (jsonUrl) => {
+  let projects = await axios.get(jsonUrl).then(res => res.data)
+  fx.rates = await axios.get('https://openexchangerates.org/api/latest.json?app_id=ec60cf523d0f4912bc160406c1703489').then(res => res.data.rates)
+  fx.base = 'USD'
+
+  return map((project) => {
+    forEachObjIndexed((value, type) => {
+      project.types[type].minPrice.TRY = Math.ceil(fx.convert(project.types[type].minPrice.USD, {from: 'USD', to: 'TRY'}))
+      project.types[type].maxPrice.TRY = Math.ceil(fx.convert(project.types[type].maxPrice.USD, {from: 'USD', to: 'TRY'}))
+    }, project.types)
+    return project
+  }, projects)
+}
 
 const getProject = (id, projects) => {
   return find(propEq('id', String(id)))(projects)
@@ -12,10 +25,6 @@ const getTypes = (project) => {
     if (accum === '') return type
     return `${accum}, ${type}`
   }, '', keys(project.types || {}))
-}
-
-const addCurrencies = async (projects) => {
-
 }
 
 const submitForm = async (data) => {
